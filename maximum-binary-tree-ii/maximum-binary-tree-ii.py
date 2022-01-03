266a531246ad35707e43a1ff1d5b733b4bac5fcb
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-03 09:34:52 +0530

    Time: 58 ms (5.47%), Space: 14.3 MB (46.88%) - LeetHub

--- maximum-binary-tree-ii/maximum-binary-tree-ii.py
+++ maximum-binary-tree-ii/maximum-binary-tree-ii.py
@@ -5,28 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorder(self,root,l):
-        if root is None:
-            return 
-        self.inorder(root.left,l)
-        l.append(root.val)
-        self.inorder(root.right,l)
-        return l
-    
-    def createRoot(self,a):
-        stack = []
-        
-        for i in a:
-            n = TreeNode(i)
-            while stack and stack[-1].val < i:
-                n.left = stack.pop()
-                
-            if stack:
-                stack[-1].right = n
-            stack.append(n)
-        return stack[0]
-        
     def insertIntoMaxTree(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        a = self.inorder(root,[])
-        a.append(val)
-        return self.createRoot(a)
+        if root is None:
+            node = TreeNode(val)
+            return node
+        
+        if val > root.val:
+            node = TreeNode(val)
+            node.left = root
+            return node
+        
+        root.right = self.insertIntoMaxTree(root.right,val)
+        return root
