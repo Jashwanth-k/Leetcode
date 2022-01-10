1bce3899b01020741c8202367bef009cfa64933e
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-10 14:14:03 +0530

    Time: 117 ms (23.73%), Space: 15.6 MB (76.12%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -3,15 +3,13 @@ class Solution:
         def allPathsHelper(sv,ev,graph,output,ans):
             if sv == ev:
                 output.append(ans)
+                return
 
             for i in graph[sv]:
                 allPathsHelper(i,ev,graph,output,ans + [i])
             return output
 
-        def allPaths(graph):
-            sv = 0
-            ev = len(graph)-1
-            ans = allPathsHelper(sv,ev,graph,[],[sv])
-            return ans
-        return allPaths(graph)
+        sv = 0
+        ev = len(graph)-1
+        return allPathsHelper(sv,ev,graph,[],[sv])
         
