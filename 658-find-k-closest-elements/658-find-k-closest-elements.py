c3610873f7a294dae00e67bb7b1156a2bb176842
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-12 14:16:56 +0530

    Time: 740 ms (5.00%), Space: 15.9 MB (14.48%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,7 +1,5 @@
 class Solution:
-    def binarySearch(self,arr,target):
-        s = 0
-        e = len(arr) - 1
+    def binarySearch(self,s,e,arr,target):
         while s <= e:
             mid = s + (e-s) // 2
             if arr[mid] == target:
@@ -17,25 +15,23 @@ class Solution:
         return s,e
         
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        mid = self.binarySearch(arr,x)
+        mid = self.binarySearch(0,len(arr)-1,arr,x)
         if abs(arr[mid[0]] - x) < abs(arr[mid[1]] - x):
             idx = mid[0]
         else:
             idx = mid[1]
-        output = []
+        output = [arr[idx]]
         l = idx - 1
         r = idx + 1
-        output.append(arr[idx])
         
         while len(output) < k:
-            if r >= len(arr) or (l >= 0 and abs(arr[l] - x) <= abs(arr[r] - x)):
+            if r >= len(arr) or (abs(arr[l] - x) <= abs(arr[r] - x)):
                 output = [arr[l]] + output
                 if len(output) == k:
                     break
                 l -= 1
-            elif r < len(arr):
+            else:
                 output.append(arr[r])
                 r += 1
-
         return output
         
