b582d8179ebf0b626404d76893c6798b51ee2ef1
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-15 16:30:10 +0530

    Time: 128 ms (10.04%), Space: 14.8 MB (67.34%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -1,25 +1,21 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        def dfs(u,v):
-            visited = set()
-            stack = [u]
-            while stack:
-                node = stack.pop()
-                if node == v:
-                    return True
-                visited.add(node)
-
-                for nei in d[node]:
-                    if nei not in visited:
-                        stack.append(nei)
-            return False
+        def dfs(u,v,visited):
+            visited.add(u)
+            if u == v:
+                return True
+            for sib in d[u]:
+                if sib not in visited:
+                    if dfs(sib,v,visited):
+                        return True
+            return
 
         d = defaultdict(list)
 
         redundant = []
         for u, v in edges:
             if u in d and v in d:
-                if dfs(u,v):
+                if dfs(u,v,set()):
                     redundant.append(u)
                     redundant.append(v)
             d[u].append(v)
