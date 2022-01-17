00e105447a03b3c6e6137254a5da86f02f365377
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-17 12:46:50 +0530

    Time: 116 ms (36.54%), Space: 15.5 MB (75.95%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,31 +1,33 @@
+import queue
 class Solution:
-    def DFS(self,sv,vs,dvs,d,stack):
-        vs[sv] = True
-        dvs[sv] = True
-        
-        for sib in d[sv]:
-            if vs[sib] == False:
-                if self.DFS(sib,vs,dvs,d,stack) == True:
-                    return True
-            else:
-                if vs[sib] == True and dvs[sib] == True:
-                    return True
-        dvs[sv] = False
-        stack.append(sv)
+    def BFS(self,q,d,deg,topo):
+        while not q.empty():
+            curr = q.get()
+            topo.append(curr)
+            
+            for sib in d[curr]:
+                deg[sib] -= 1
+                if deg[sib] == 0:
+                    q.put(sib)
                 
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
         d = collections.defaultdict(list)
         for u,v in prerequisites:
             d[u].append(v)
-            
-        vs = [False] * numCourses
-        dvs = [False] * numCourses
-        output = []
-        stack = []
         
-        for i in range(numCourses):
-            if vs[i] == False:
-                if self.DFS(i,vs,dvs,d,stack):
-                    return []
-        return stack
+        deg = [0] * numCourses
+        for i in d:
+            for sib in d[i]:
+                deg[sib] += 1
+        
+        topo = []
+        q = queue.Queue()
+        for j in range(numCourses):
+            if deg[j] == 0:
+                q.put(j)
+                
+        self.BFS(q,d,deg,topo)
+        if len(topo) != numCourses:
+            return []
+        return topo[::-1]
