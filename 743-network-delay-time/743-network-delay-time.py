f6e0f84f71eae4a505ce0285725c24606f8a5a73
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-28 20:08:12 +0530

    Time: 955 ms (14.12%), Space: 16.7 MB (6.47%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,31 +1,21 @@
-import queue
+import heapq
 class Solution:
-    def BFS(self,k,d,times,dist):
-        q = queue.Queue()
-        q.put(k)
-        dist[k] = 0
-        
-        while not q.empty():
-            curr = q.get()
-            for sib,wt in d[curr]:
-                if dist[curr] + wt < dist[sib]:
-                    dist[sib] = dist[curr] + wt
-                    q.put(sib)
-        return
-    
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         d = collections.defaultdict(list)
         for i,j,w in times:
             d[i].append((j,w))
-        
-        dist = [float('inf')] * (n+1)
-        self.BFS(k,d,times,dist)
-        
-        maxim = float('-inf')
+            
+        dist = [float('inf')]*(n+1)
+        dist[k] = 0
+        heap = [(0,k)]
+        while heap:
+            curr = heapq.heappop(heap)
+            ndis,node = curr[0],curr[1]
+            for sib,wt in d[node]:
+                currdis = ndis + wt
+                if currdis < dist[sib]:
+                    dist[sib] = currdis
+                    heapq.heappush(heap,(currdis,sib))
         dist.pop(0)
-        for i in dist:
-            if i == float('inf'):
-                return -1
-            maxim = max(maxim,i)
-        return maxim
-        
+        ans = max(dist)
+        return -1 if ans == float('inf') else ans
