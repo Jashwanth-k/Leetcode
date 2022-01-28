23609299dae908137df793da61662c6cea05135b
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-01-28 21:11:00 +0530

    Time: 715 ms (60.62%), Space: 15.3 MB (56.56%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        d = collections.defaultdict(list)
+        for i,j,k in edges:
+            d[i].append((j,k))
+            d[j].append((i,k))
+            
+        s = collections.defaultdict(list)
+        for i in range(n):
+            heap = [(0, i)]
+            dist = [float('inf')] * n
+            while heap:
+                curr = heap.pop(0)
+                ndis, node = curr[0], curr[1]
+                for sib, wt in d[node]:
+                    currdis = ndis + wt
+                    if currdis < dist[sib] and currdis <= distanceThreshold:
+                        if sib not in s[i] and sib != i:
+                            s[i].append(sib)
+                        dist[sib] = currdis
+                        heapq.heappush(heap, (currdis, sib))
+        
+        maxim = float('inf')
+        ans = float('-inf')
+        for i in range(n):
+            if len(s[i]) <= maxim:
+                maxim = len(s[i])
+                ans = max(ans,i)
+        return ans
