04bba0c2cb9ca1bd59affe8160114ee945b78aa6
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-02-02 10:37:04 +0530

    Time: 1798 ms (5.15%), Space: 14.8 MB (91.06%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -6,10 +6,9 @@ class Solution:
         ans = float('inf')
         while not q.empty():
             r,c,moves = q.get()
-            maze[r][c] = '+'
             if moves > 0 and (r == n-1 or c == m-1 or r == 0 or c == 0):
                 ans = min(ans,moves)
-                continue
+                return ans
 
             if c+1 < m and maze[r][c+1] != '+':
                 q.put([r,c+1,moves+1])
@@ -31,5 +30,6 @@ class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         n = len(maze)
         m = len(maze[0])
+        maze[entrance[0]][entrance[1]] = '+'
         ans = self.explore(maze,entrance,n,m)
         return -1 if ans == float('inf') else ans
