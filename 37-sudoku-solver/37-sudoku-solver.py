dad833b8db0d74022b8021e75d0c1d17dbc9aa48
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-02-03 12:19:01 +0530

    Time: 608 ms (36.22%), Space: 14.1 MB (93.66%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isSafe(self,board,r,c,n,nbr):
+        # checking row
+        for i in range(n):
+            if i != c and board[r][i] == nbr:
+                return False
+            
+        # checking column
+        for i in range(n):
+            if i != r and board[i][c] == nbr:
+                return False
+            
+        # checking 3x3 sub-box
+        subrow = r // 3 * 3
+        subcol = c // 3 * 3
+        for i in range(3):
+            for j in range(3):
+                if (i != r and j != c) and board[subrow + i][subcol + j] == nbr:
+                    return False
+        return True
+    
+    def solveSudokuHelper(self,board,r,c,n):
+        if r >= n:
+            return board
+        
+        while c < n and board[r][c] != '.':
+            c += 1
+            
+        if c >= n:
+            ans = self.solveSudokuHelper(board,r+1,0,n)
+            if ans: return ans
+            
+        for i in range(1,10):
+            nbr = str(i)
+            if self.isSafe(board,r,c,n,nbr):
+                board[r][c] = nbr
+                ans = self.solveSudokuHelper(board,r,c+1,n)
+                if ans: return ans
+                board[r][c] = '.'
+        return
+
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        self.solveSudokuHelper(board,0,0,9)
+        
