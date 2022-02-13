2a107278f3adcd9b2d3e348b5af86ecf699e859b
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-02-13 14:22:22 +0530

    Time: 62 ms (8.53%), Space: 13.8 MB (92.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -7,11 +7,8 @@ class Solution:
         suboutput = smalloutput[numRows-2]
         ans = [0] * numRows
         ans[0],ans[-1] = suboutput[0],suboutput[-1]
-        i,j = 0,1
-        while j < len(suboutput):
-            ans[j] = suboutput[i] + suboutput[j]
-            i += 1
-            j += 1
+        for i in range(1,len(suboutput)):
+            ans[i] = suboutput[i] + suboutput[i-1]
             
         smalloutput.append(ans)
         return smalloutput
