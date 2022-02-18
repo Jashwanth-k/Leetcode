8fc2ce183ac73fb3b9b3384abbdaab8af77fb7e8
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-02-18 12:01:22 +0530

    Time: 172 ms (65.88%), Space: 15.3 MB (7.53%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,23 +1,28 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n,m = len(s),len(p)
-        if m > n: return []
+        anagLen,strLen = len(p),len(s)
+        if anagLen > strLen:
+            return []
         i = j = 0
         output = []
-        dwindow = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-        danag = {}
+        mapAnag = {}
         for k in p:
-            danag[k] = danag.get(k,0) + 1
+            mapAnag[k] = mapAnag.get(k,0) + 1
+        count = len(mapAnag)
+        
+        while j < strLen:
+            if s[j] in mapAnag:
+                mapAnag[s[j]] -= 1
+                if mapAnag[s[j]] == 0:
+                    count -= 1
             
-        while j < n:
-            dwindow[s[j]] += 1
-            if (j-i+1) == m:
-                for k in p:
-                    if dwindow[k] != danag[k]:
-                        break
-                else:
+            if (j-i+1) == anagLen:
+                if count == 0:
                     output.append(i)
-                dwindow[s[i]] -= 1
+                if s[i] in mapAnag:
+                    if mapAnag[s[i]] == 0:
+                        count += 1
+                    mapAnag[s[i]] += 1
                 i += 1
                 j += 1
             else:
