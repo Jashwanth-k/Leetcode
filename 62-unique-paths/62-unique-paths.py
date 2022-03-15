abf7ea60b4aedbd49972b2ddcf2b6efc08059445
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-15 14:37:25 +0530

    Time: 32 ms (88.60%), Space: 13.8 MB (83.41%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,24 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def uniquePathsHelper(i, j, m, n, grid):
-            if i == m - 1 and j == n - 1:
+        def uniquePathsHelper(m,n,i,j,dp):
+            if i == m-1 and j == n-1:
                 return 1
-
+            
             if i >= m or j >= n:
                 return 0
-
-            ans1 = ans2 = 0
-            if grid[i][j+1] == 0:
-                ans1 = uniquePathsHelper(i, j + 1, m, n, grid)
-            else:
-                ans1 = grid[i][j+1]
-            if grid[i+1][j] == 0:
-                ans2 = uniquePathsHelper(i + 1, j, m, n, grid)
-            else:
-                ans2 = grid[i+1][j]
-            grid[i][j] = ans1 + ans2
-            return ans1 + ans2
-
-        grid = [[0 for i in range(m+100)] for j in range(n+100)]
-        return uniquePathsHelper(0, 0, m, n, grid)
+            
+            if dp[i][j] != -1: return dp[i][j]
+            
+            right = uniquePathsHelper(m,n,i,j+1,dp)
+            down = uniquePathsHelper(m,n,i+1,j,dp)
+            dp[i][j] = right+down
+            return dp[i][j]
         
+        dp = [[-1]*n for i in range(m)]
+        return uniquePathsHelper(m,n,0,0,dp)
+    
