a550f3d78f011c98a217da9ef83921ae904e970d
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-15 14:24:31 +0530

    Time: 80 ms (73.47%), Space: 15 MB (72.29%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,17 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        def minimumTotalHelper(triangle,dp,idx = 0,i = 0):
-            if i == len(triangle)-1:
-                return triangle[i][idx]
-            
-            if dp[i][idx] != -1:
-                return dp[i][idx]
-            
-            left = triangle[i][idx] + minimumTotalHelper(triangle,dp,idx,i+1)
-            right = triangle[i][idx] + minimumTotalHelper(triangle,dp,idx+1,i+1)
-            dp[i][idx] = min(left,right)
-            return dp[i][idx]
-        
-        dp = [[-1]*i for i in range(1,len(triangle)+1)]
-        return minimumTotalHelper(triangle,dp)
-            
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] = triangle[i][j] + min(triangle[i+1][j],triangle[i+1][j+1])
+        return triangle[0][0]
+                
