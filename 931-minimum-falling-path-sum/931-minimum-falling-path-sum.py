a2689d8112f90bd361ea0320e4e491d1152b5453
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-16 12:58:10 +0530

    Time: 3481 ms (5.08%), Space: 14.7 MB (73.34%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,24 +1,19 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        def fallingHelper(matrix,n,i,j,dp):
-            if j < 0 or j >= n or i < 0:
-                return float('inf')
-            
-            if i == 0:
-                return matrix[i][j]
-            
-            if dp[i][j] != -1 : return dp[i][j]
-            
-            rightdia = fallingHelper(matrix,n,i-1,j+1,dp)
-            up = fallingHelper(matrix,n,i-1,j,dp)
-            leftdia = fallingHelper(matrix,n,i-1,j-1,dp)
-            dp[i][j] = matrix[i][j] + min(leftdia,up,rightdia)
-            return dp[i][j]
-        
         n = len(matrix)
-        dp = [[-1]*n for j in range(n)]
+        if n == 1:
+            return min(matrix[0])
+        
+        dp = [[float('inf')]*(n+1) for j in range(n+1)]
         ans = float('inf')
-        for k in range(n):
-            currans = fallingHelper(matrix,n,n-1,k,dp)
-            ans = min(currans,ans)
+        for st in range(n-1,-1,-1):
+            dp[n-1][st] = matrix[n-1][st]
+            for i in range(n-2,-1,-1):
+                for j in range(n-1,-1,-1):
+                    curr = min(dp[i+1][j],dp[i+1][j+1],dp[i+1][j-1])
+                    dp[i][j] = matrix[i][j] + curr
+                    if i == 0:
+                        ans = min(ans,dp[i][j])
+            dp[n-1][st] = float('inf')
         return ans
+                    
