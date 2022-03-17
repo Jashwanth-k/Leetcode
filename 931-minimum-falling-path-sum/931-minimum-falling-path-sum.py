0cc4d3dfb4b92809a7e4733f27a6ab1a0b320fc6
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-17 17:46:12 +0530

    Time: 128 ms (97.20%), Space: 14.6 MB (92.44%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -3,17 +3,17 @@ class Solution:
         n = len(matrix)
         if n == 1:
             return min(matrix[0])
-        
-        dp = [[float('inf')]*(n+1) for j in range(n+1)]
+
+        dp = [[float('inf')] * (n + 1) for j in range(n + 1)]
+        dp[-2] = matrix[-1]
+        dp[-2].append(float('inf'))
+
         ans = float('inf')
-        for st in range(n-1,-1,-1):
-            dp[n-1][st] = matrix[n-1][st]
-            for i in range(n-2,-1,-1):
-                for j in range(n-1,-1,-1):
-                    curr = min(dp[i+1][j],dp[i+1][j+1],dp[i+1][j-1])
-                    dp[i][j] = matrix[i][j] + curr
-                    if i == 0:
-                        ans = min(ans,dp[i][j])
-            dp[n-1][st] = float('inf')
+        for i in range(n - 2, -1, -1):
+            for j in range(n - 1, -1, -1):
+                curr = min(dp[i + 1][j], dp[i + 1][j + 1], dp[i + 1][j - 1])
+                dp[i][j] = matrix[i][j] + curr
+                if i == 0:
+                    ans = min(ans, dp[i][j])
         return ans
                     
