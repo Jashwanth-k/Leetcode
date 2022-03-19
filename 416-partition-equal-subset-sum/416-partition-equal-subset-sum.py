06cc78008c9d68531da8df80a63998c3aee958a5
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-19 11:28:48 +0530

    Time: 4369 ms (13.05%), Space: 26.8 MB (52.70%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,22 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        def partitionHelper(nums,k,i):
-            if k == 0:
-                return True
-            
-            if k < 0 or i < 0:
-                return False
-            
-            if dp[i][k] != -1: return dp[i][k]
-            
-            inclu = partitionHelper(nums,k-nums[i],i-1)
-            exclu = partitionHelper(nums,k,i-1)
-            dp[i][k] = inclu or exclu
-            return dp[i][k]
-        
-        n,s = len(nums),sum(nums)
+        s,n = sum(nums),len(nums)
         if s % 2 != 0:
             return False
         k = s // 2
-        dp = [[-1]*(k+1) for j in range(n)]
-        return partitionHelper(nums,k,n-1)
+        
+        dp = [[0]*(k+1) for j in range(n)]
+        for i in range(n): dp[i][0] = True
+        for j in range(1,k+1) : dp[0][j] = False
+        
+        for idx in range(1,n):
+            for target in range(1,k+1):
+                inclu = False
+                if target >= nums[idx] : inclu = dp[idx-1][target-nums[idx]]
+                exclu = dp[idx-1][target]
+                dp[idx][target] = inclu or exclu
+        return dp[-1][-1]
