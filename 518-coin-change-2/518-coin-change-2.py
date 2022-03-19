db1c5a07dce6adafc8fd810ae30c8c4aa3fe5b43
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-19 12:09:46 +0530

    Time: 975 ms (17.94%), Space: 34.9 MB (22.85%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,19 +1,14 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def changeHelper(coins,amount,dp,i):
-            if amount == 0:
-                return 1
-            
-            if amount < 0 or i < 0:
-                return 0
-            
-            if dp[i][amount] != -1: return dp[i][amount]
-            
-            inclu = changeHelper(coins,amount-coins[i],dp,i)
-            exclu = changeHelper(coins,amount,dp,i-1)
-            dp[i][amount] = inclu + exclu
-            return dp[i][amount]
-        
         n = len(coins)
-        dp = [[-1]*(amount+1) for j in range(n)]
-        return changeHelper(coins,amount,dp,n-1)
+        dp = [[0] * (amount + 1) for j in range(n)]
+
+        for i in range(n): dp[i][0] = 1
+        for i in range(n):
+            for target in range(1, amount + 1):
+                inclu = 0
+                if target >= coins[i]:
+                    inclu = dp[i][target - coins[i]]
+                exclu = dp[i - 1][target]
+                dp[i][target] = inclu + exclu
+        return dp[-1][-1]
