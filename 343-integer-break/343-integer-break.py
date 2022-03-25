143c44a43600f5996a3d0bf799cafb5fb3927a9b
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-25 11:19:30 +0530

    Time: 36 ms (83.06%), Space: 13.8 MB (97.59%) - LeetHub

--- 343-integer-break/343-integer-break.py
+++ 343-integer-break/343-integer-break.py
@@ -1,19 +1,15 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        def breakHelper(n,st = 0):
-            if n == 0:
-                return 1
-            
-            if dp[n] != -1: return dp[n]
+        dp = [0 for i in range(n + 1)]
+        dp[0] = dp[1] = 1
 
-            ans = float('-inf')
-            m = n
-            if st == 0: m = n-1
-            for i in range(m, 0, -1):
-                pick = i * breakHelper(n - i,1)
-                ans = max(pick,ans)
-            dp[n] = ans
-            return ans
+        ans = float('-inf')
+        for i in range(2, n + 1):
+            for j in range(i, 0, -1):
+                if j == i: j -= 1
+                pick1 = j * dp[i - j]
+                pick2 = j * (i-j)
+                ans = max(pick1,pick2, ans)
+            dp[i] = ans
+        return ans
         
-        dp = [-1 for i in range(n+1)]
-        return breakHelper(n)
