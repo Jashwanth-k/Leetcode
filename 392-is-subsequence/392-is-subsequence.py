ec4f84162ddff0a81c5c18c063b41b756d8f10e6
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-26 20:00:26 +0530

    Time: 59 ms (22.33%), Space: 24.5 MB (5.23%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -2,20 +2,15 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) > len(t):
             return False
-        def Helper(s, t, i, j, dp):
+        def Helper(s, t, i, j):
             if i < 0 or j < 0:
                 return 0
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
 
             if s[i] == t[j]:
-                ans = 1 + Helper(s, t, i - 1, j - 1, dp)
+                ans = 1 + Helper(s, t, i - 1, j - 1)
             else:
-                ans = Helper(s, t, i, j-1, dp)
-            dp[i][j] = ans
+                ans = Helper(s, t, i, j-1)
             return ans
-
+        
         n, m = len(s), len(t)
-        dp = [[-1]*(m) for j in range(n)]
-        return Helper(s, t, n - 1, m - 1, dp) == n
+        return Helper(s, t, n - 1, m - 1) == n
