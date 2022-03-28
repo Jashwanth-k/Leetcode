5f0c175e5f17d001a2ed3b3d19b0b9203c808aa7
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-28 10:24:32 +0530

    Time: 396 ms (54.08%), Space: 67.1 MB (35.21%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.py
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -1,21 +1,16 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        def reachHelper(arr, start, n, vl,vr):
+        def reachHelper(arr, start, n):
+            if start < 0 or start >= n or arr[start] < 0:
+                return False
             if arr[start] == 0:
                 return True
 
-            right = left = False
-            rightind = start + arr[start]
-            leftind = start - arr[start]
-            if rightind < n and vr[rightind] == False:
-                vr[rightind] = True
-                right = reachHelper(arr, start + arr[start], n,vl,vr)
-            if leftind >= 0 and vl[leftind] == False:
-                vl[leftind] = True
-                left = reachHelper(arr, start - arr[start], n,vl,vr)
+            val = arr[start]
+            arr[start] = -arr[start]
+            right = reachHelper(arr, start + val, n)
+            left = reachHelper(arr, start - val, n)
             return right or left
 
         n = len(arr)
-        vl = [False] * n
-        vr = [False] * n
-        return reachHelper(arr, start, n,vl,vr)
+        return reachHelper(arr, start, n)
