26eee74ae7f9935eff4cf23cd30ca625faed517f
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-03-28 09:39:47 +0530

    Time: 7779 ms (21.67%), Space: 15 MB (90.79%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,22 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        def jumpHelper(nums,i,n,dp):
-            if i == n-1:
-                return 0
-            if nums[i] == 0:
-                return float('inf')
-            
-            if dp[i] != -1:
-                return dp[i]
-            
-            ans = float('inf')
-            limit = min(nums[i],n-i-1)
-            for jump in range(1,limit+1):
-                currans = 1 + jumpHelper(nums,i+jump,n,dp)
-                ans = min(currans,ans)
-            dp[i] = ans
-            return dp[i]
-        
         n = len(nums)
-        dp = [-1]*(n)
-        return jumpHelper(nums,0,n,dp)
+        dp = [0]*n
+        
+        for i in range(n-2,-1,-1):
+            ans = float('inf')
+            for jump in range(1,min(nums[i],n-i-1)+1):
+                curans = 1 + dp[i + jump]
+                ans = min(curans,ans)
+            dp[i] = ans
+        return dp[0]
