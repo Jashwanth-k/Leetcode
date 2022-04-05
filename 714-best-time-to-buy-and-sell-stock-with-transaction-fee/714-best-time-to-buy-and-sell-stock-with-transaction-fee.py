4d81b16dac23b45065c94c27d134352a4a1c2133
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-04-05 18:01:04 +0530

    Time: 1522 ms (22.75%), Space: 23.3 MB (26.50%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,23 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        def profitHelper(prices,fee,i,buy,n,dp):
-            if i == n:
-                return 0
-            
-            if dp[i][buy] != -1:
-                return dp[i][buy]
-            
-            if buy == 1:
-                pick = -prices[i] + profitHelper(prices,fee,i+1,0,n,dp)
-                unpick = profitHelper(prices,fee,i+1,1,n,dp)
-                dp[i][buy] = max(pick,unpick)
-                return dp[i][buy]
-            else:
-                sell = prices[i] - fee + profitHelper(prices,fee,i+1,1,n,dp)
-                unsell = profitHelper(prices,fee,i+1,0,n,dp)
-                dp[i][buy] = max(sell,unsell)
-                return dp[i][buy]
-                
         n = len(prices)
-        dp = [[-1]*(2) for j in range(n)]
-        return profitHelper(prices,fee,0,1,n,dp)
+        dp = [[0]*(2) for j in range(n+1)]
+
+        for i in range(n-1,-1,-1):
+            pick = -prices[i] + dp[i+1][0]
+            unpick = dp[i+1][1]
+            dp[i][1] = max(pick,unpick)
+            
+            sell = prices[i] - fee + dp[i+1][1]
+            unsell = dp[i+1][0]
+            dp[i][0] = max(sell,unsell)
+        return dp[0][1] 
