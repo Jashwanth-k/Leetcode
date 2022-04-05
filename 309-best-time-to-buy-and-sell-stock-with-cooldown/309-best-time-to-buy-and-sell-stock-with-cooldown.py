e63526c25f2dae85b2a3fedee17118954148681c
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-04-05 16:52:13 +0530

    Time: 90 ms (18.47%), Space: 17.3 MB (32.75%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,28 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def profitHelper(prices,i,buy,cool,n,dp):
-            if i == n:
+        def profitHelper(prices,i,buy,n,dp):
+            if i >= n:
                 return 0
-            if dp[i][buy][cool] != -1:
-                return dp[i][buy][cool]
+            if dp[i][buy] != -1:
+                return dp[i][buy]
             
-            ans1 = ans2 = ans3 = float('-inf')
-            if cool == 1:
-                ans1 = profitHelper(prices,i+1,buy,0,n,dp)
-            
-            elif buy == 1:
-                pick = -prices[i] + profitHelper(prices,i+1,0,cool,n,dp)
-                unpick = profitHelper(prices,i+1,1,cool,n,dp)
-                ans2 = max(pick,unpick)
+            ans1 = ans2 = float('-inf')
+            if buy == 1:
+                pick = -prices[i] + profitHelper(prices,i+1,0,n,dp)
+                unpick = profitHelper(prices,i+1,1,n,dp)
+                ans1 = max(pick,unpick)
             else:
-                sell = prices[i] + profitHelper(prices,i+1,1,1,n,dp)
-                unsell = profitHelper(prices,i+1,0,cool,n,dp)
-                ans3 = max(sell,unsell)
+                sell = prices[i] + profitHelper(prices,i+2,1,n,dp)
+                unsell = profitHelper(prices,i+1,0,n,dp)
+                ans2 = max(sell,unsell)
 
-            dp[i][buy][cool] = max(ans1,ans2,ans3)
-            return dp[i][buy][cool]
+            dp[i][buy] = max(ans1,ans2)
+            return dp[i][buy]
         
         n = len(prices)
-        dp = [[[-1]*(2) for j in range(2)] for i in range(n)]
-        return profitHelper(prices,0,1,0,n,dp)
+        dp = [[-1]*(2) for i in range(n)]
+        return profitHelper(prices,0,1,n,dp)
     
