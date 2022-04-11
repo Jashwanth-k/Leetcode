0c77e85cba4cbce7d9de5c5d0d8062151ed6a374
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-04-11 17:29:57 +0530

    Time: 384 ms (12.37%), Space: 14.4 MB (37.12%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,35 +1,23 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        def ballHelper(grid,i,j,n,m,dp):
-            if i == n:
-                return j
-            
-            if grid[i][j] == 1 and j + 1 >= m:
-                return -1
-            if grid[i][j] == -1 and j - 1 < 0:
-                return -1 
-            if grid[i][j] == 1 and grid[i][j+1] == -1:
-                return -1
-            if grid[i][j] == -1 and grid[i][j-1] == 1:
-                return -1
-            
-            if dp[i][j] != -2:
-                return dp[i][j]
-            
-            right = left = float('-inf')
-            if grid[i][j] == 1:
-                right = ballHelper(grid,i+1,j+1,n,m,dp)
-            if grid[i][j] == -1:
-                left = ballHelper(grid,i+1,j-1,n,m,dp)
-                
-            dp[i][j] = max(left,right)
-            return dp[i][j]
-        
         n,m = len(grid),len(grid[0])
+        
+        def DFS(grid,r,c):
+            col = grid[r][c]
+            nextcol = col + c
+        
+            if nextcol >= m or nextcol < 0:
+                return -1
+            if col == 1 and grid[r][c] != grid[r][c+1]:
+                return -1
+            if col == -1 and grid[r][c] != grid[r][c-1]:
+                return -1
+            if r == n-1:
+                return nextcol
+            return DFS(grid,r+1,nextcol)
+        
         output = []
-        dp = [[-2]*(m) for j in range(n)]
-        for k in range(m):
-            ans = ballHelper(grid,0,k,n,m,dp)
+        for i in range(m):
+            ans = DFS(grid,0,i)
             output.append(ans)
         return output
-                
