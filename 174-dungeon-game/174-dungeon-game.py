31d1c5ebd99e2a3f13ff807afbdcd9cda13516d9
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-04-15 18:46:12 +0530

    Time: 72 ms (96.33%), Space: 15 MB (98.35%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,27 +1,17 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        def calcHelper(dungeon,i,j,n,m,dp):
-            if i == n-1 and j == m-1:
-                if dungeon[i][j] < 0:
-                    return 1 + -dungeon[i][j]
-                return 1
-            if i >= n or j >= m:
-                return float('inf')
-            
-            if dp[i][j] != float('inf'):
-                return dp[i][j]
-            
-            right = calcHelper(dungeon,i,j+1,n,m,dp)
-            down = calcHelper(dungeon,i+1,j,n,m,dp)
-            
-            ans = min(right,down) - dungeon[i][j]
-            if ans <= 0:
-                dp[i][j] = 1
-            else:
-                dp[i][j] = ans
-            return dp[i][j]
-        
         n,m = len(dungeon),len(dungeon[0])
-        dp = [[float('inf')]*(m) for j in range(n)]
-        return calcHelper(dungeon,0,0,n,m,dp)
-    
+        dp = [[float('inf')]*(m+1) for j in range(n+1)]
+        last = dungeon[-1][-1]
+        dp[n-1][m-1] = 1+(-last) if last < 0 else 1
+        
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if i == n-1 and j == m-1:
+                    continue
+                right = dp[i][j+1]
+                down = dp[i+1][j]
+                ans = min(right,down) - dungeon[i][j]
+                dp[i][j] = 1 if ans <= 0 else ans
+        return dp[0][0]
+                
