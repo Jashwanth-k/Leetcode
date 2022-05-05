5bf05661ee5d7f078cd9bd51bc68ac13f24973c0
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-05 12:20:04 +0530

    Time: 88 ms (6.20%), Space: 14.6 MB (30.64%) - LeetHub

--- 900-rle-iterator/900-rle-iterator.py
+++ 900-rle-iterator/900-rle-iterator.py
@@ -1,24 +1,32 @@
 class RLEIterator:
+
     def __init__(self, encoding: List[int]):
-        self.d = []
-        self.idx = 0
-        prev = 0
+        self.d = {}
+        self.nbr = 0
+        count = 0
         for i in range(0,len(encoding)-1,2):
             freq,val = encoding[i],encoding[i+1]
             if freq == 0: continue
-            pairs = [val,prev+1,prev+freq]
-            self.d.append(pairs)
-            prev = pairs[2]
+            self.d[count + freq] = val
+            count += freq
+        self.bs = list(self.d.keys())
+        self.len = len(self.bs)
 
     def next(self, n: int) -> int:
-        self.idx += n
-        val = -1
-        for i in range(len(self.d)):
-            key,start,end = self.d[i]
-            if start <= self.idx <= end:
-                val = key
-                self.d[i] = [key,self.idx,end]
-        return val
+        self.nbr += n
+        si = 0
+        ei = self.len - 1
+        while si <= ei:
+            mid = si + (ei - si) // 2
+            if self.nbr == self.bs[mid]:
+                return self.d[self.bs[mid]]
+            if self.nbr > self.bs[mid]:
+                si = mid + 1
+            else:
+                ei = mid - 1
+                
+        if si >= self.len:  return -1
+        return self.d[self.bs[si]]
 
 # Your RLEIterator object will be instantiated and called as such:
 # obj = RLEIterator(encoding)
