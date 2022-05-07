16679cf7d990dfc32c6996b322c611d517b275f6
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-07 17:57:57 +0530

    Time: 70 ms (98.77%), Space: 14.5 MB (57.56%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -1,25 +1,9 @@
-class Solution:    
+class Solution:
     def longestWord(self, words: List[str]) -> str:
-        n = len(words)
         ans = ""
-        maxLen = 0
-        for i in range(n):
-            word1 = words[i]            
-            x = len(word1)
-            if x < maxLen:
-                continue
-            
-            helper = words[i]
-            while len(helper) != 0:
-                helper = helper[:-1]
-                if helper not in words:
-                    break
-                    
-            if len(helper) == 0:
-                if x > maxLen:
-                    maxLen = x
-                    ans = word1
-                elif x == maxLen:
-                    ans = min(ans,word1)
+        wordDict = set(words)
+        for word in words:
+            if len(word) > len(ans) or (len(word) == len(ans) and word < ans):
+                if all(word[:k] in wordDict for k in range(1,len(word))):
+                    ans = word
         return ans
-                
