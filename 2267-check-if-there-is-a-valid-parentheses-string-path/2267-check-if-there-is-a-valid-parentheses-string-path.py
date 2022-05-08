184a3d8d6424bab834f65683dc175474f7fa45a7
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-08 17:59:54 +0530

    Time: 8853 ms (10.00%), Space: 500.5 MB (10.00%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.py
@@ -1,31 +1,23 @@
 class Solution:
     def hasValidPath(self, grid: List[List[str]]) -> bool:
-        def BFS(grid,n,m):
-            q = collections.deque()
-            visited = set()
-            if grid[0][0] == "(":
-                q.append((1,0,0,0))
-                visited.add((1,0,0,0))
-
-            while len(q) != 0:
-                leftc,rightc,i,j = q.popleft()
-                if i == n - 1 and j == m - 1 and leftc == rightc:
-                    return True
-
-                paths = [[i, j + 1], [i + 1, j]]
-                for r, c in paths:
-                    if r < n and c < m:
-                        nextCell = grid[r][c]
-                        newl,newr = leftc,rightc
-                        if nextCell == ")":
-                            newr += 1
-                        else:
-                            newl += 1
-                        if newl >= newr and (newl, newr, r, c) not in visited:
-                            q.append((newl, newr, r, c))
-                            visited.add((newl, newr, r, c))
-            return False
+        def DFS(grid,i,j,n,m,k):
+            if i >= n or j >= m:
+                return False
+            if grid[i][j] == "(":  k += 1
+            else:  k -= 1
+            if k < 0:
+                return False
+            if i == n-1 and j == m-1:
+                return k == 0
+            if (i,j,k) in self.dp:
+                return self.dp[i,j,k]
+            
+            right = DFS(grid,i,j+1,n,m,k)
+            down = DFS(grid,i+1,j,n,m,k)
+            self.dp[i,j,k] = right or down
+            return right or down
         
         n,m = len(grid),len(grid[0])
-        return BFS(grid,n,m)
-                
+        self.dp = {}
+        if grid[0][0] == ")" or grid[-1][-1] == "(":  return False
+        return DFS(grid,0,0,n,m,0)
