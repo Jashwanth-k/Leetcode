06a4e005947a42e90264108bd9032aa0e1b9b029
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-12 09:41:52 +0530

    Time: 76 ms (67.01%), Space: 14.4 MB (47.48%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        def DFS(nums,result,path):
+        def DFS(nums,path):
             if len(nums) == 0:
-                result.append(path)
+                self.ans.append(path)
                 return
             
             for i in range(len(nums)):
                 if i > 0 and nums[i] == nums[i-1]:
                     continue
-                
-                DFS(nums[:i] + nums[i+1:],result,path + [nums[i]])
-                
-        result = []
-        DFS(nums,result,[])
-        return result
-    
+                DFS(nums[:i] + nums[i+1:],path + [nums[i]])
+        self.ans = []
+        DFS(nums,[])
+        return self.ans
