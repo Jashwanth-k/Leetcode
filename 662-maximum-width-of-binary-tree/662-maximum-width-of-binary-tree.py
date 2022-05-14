314ecb73addc8b64c493f2e887b4bdaec92cbf0e
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-14 11:29:10 +0530

    Time: 67 ms (38.79%), Space: 14.7 MB (77.22%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -5,29 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findLeft(self,root,ht,ct):
-        if root is None:
-            return 
-        self.leftd[ht] = min(self.leftd.get(ht,float('inf')),ct)
-   
-        lct = self.findLeft(root.left,ht+1,ct*2)
-        rct = self.findLeft(root.right,ht+1,(ct*2)+1)
-    
-    def findRight(self,root,ht,ct):
-        if root is None:
-            return 
-        self.rightd[ht] = min(self.rightd.get(ht,float('inf')),ct)
-            
-        lct = self.findRight(root.left,ht+1,(ct*2)+1)
-        rct = self.findRight(root.right,ht+1,ct*2)
-    
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.leftd = {}
-        self.rightd = {}
-        self.findLeft(root,0,0)
-        self.findRight(root,0,0)
-        ans = 0
-        for i in self.leftd:
-            if i in self.rightd:
-                ans = max(ans,2**i - (self.leftd[i] + self.rightd[i]))
-        return ans
+        def BFS(root):
+            q = collections.deque()
+            q.append((root,1))
+            q.append((None,None))
+            prev = None
+            while len(q) != 0:
+                curr,idx = q.popleft()
+                if prev == None or idx == None:
+                    prev = idx
+                if curr == None:
+                    if len(q) == 0:
+                        break
+                    q.append((None,None))
+                    continue
+                
+                if prev:
+                    self.ans = max(self.ans,idx - prev + 1)
+                    
+                if curr.left:
+                    q.append((curr.left,2*idx))
+                if curr.right:
+                    q.append((curr.right,2*idx+1))
+                    
+        self.ans = 1
+        BFS(root)
+        return self.ans
