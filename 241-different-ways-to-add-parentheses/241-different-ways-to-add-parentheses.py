1c4a3df65f56b5b813b3c670a55e38b159adb284
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-20 12:09:32 +0530

    Time: 58 ms (38.22%), Space: 13.9 MB (85.01%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
@@ -1,27 +1,24 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
-        def DFS(expression,i,j):
-            if i == j:
-                return [int(expression[i])]
+        def helper(a,b,exp):
+            if exp == "*":
+                return a*b
+            if exp == "+":
+                return a+b
+            if exp == "-":
+                return a-b
+
+        def DFS(expression, i, j):
+            if expression[i:j+1].isdigit():
+                return [int(expression[i:j+1])]
 
             output = []
             for k in range(i, j):
-                if expression[k] == "*":
+                if expression[k] in "+*-":
                     for l in DFS(expression, i, k - 1):
                         for m in DFS(expression, k + 1, j):
-                            output.append(l*m)
-                elif expression[k] == "-":
-                    for l in DFS(expression, i, k - 1):
-                        for m in DFS(expression, k + 1, j):
-                            output.append(l - m)
-                elif expression[k] == "+":
-                    for l in DFS(expression, i, k - 1):
-                        for m in DFS(expression, k + 1, j):
-                            output.append(l + m)
-            if output:
-                return output
-            else:
-                return [int(expression[i:j+1])]
+                            output.append(helper(l,m,expression[k]))
+            return output
                 
         n = len(expression)
         return DFS(expression,0,n-1)
