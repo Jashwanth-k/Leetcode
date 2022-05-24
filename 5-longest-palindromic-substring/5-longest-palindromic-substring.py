a42fa06b440cfe1d049e0fbe3737153cb993ed6d
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-05-24 10:38:36 +0530

    Time: 707 ms (90.89%), Space: 14 MB (30.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,49 +1,20 @@
 class Solution:
-    def longestPalinOdd(self,s,n):
-        ans = ""
-        maxLen = 0
-        for i in range(n-1):
-            r = i+1
-            l = i-1
-            if 1 > maxLen:
-                maxLen = 1
-                ans = s[i]
-            while l >= 0 and r < n:
-                if s[l] != s[r]:
-                    break
-                if r-l+1 > maxLen:
-                    maxLen = r-l+1
-                    ans = s[l:r+1]
-                l -= 1
-                r += 1
-        return ans,maxLen
-    
-    def longestPalinEven(self,s,n):
-        ans = ""
-        maxLen = 0
-        for i in range(n-1):
-            j = i+1
-            r = i+2
-            l = i-1
-            if s[i] == s[j]:
-                if 2 > maxLen:
-                    maxLen = 2
-                    ans = s[i:j+1]
-                while l >= 0 and r < n:
-                    if s[l] != s[r]:
-                        break
-                    if r-l+1 > maxLen:
-                        maxLen = r-l+1
-                        ans = s[l:r+1]
-                    l -= 1
-                    r += 1
-        return ans,maxLen
-    
     def longestPalindrome(self, s: str) -> str:
+        def helper(s,si,ei):
+            while si >= 0 and ei < n and s[si] == s[ei]:
+                si -= 1
+                ei += 1
+            return s[si+1:ei]
+        
         n = len(s)
-        if n == 1: return s[0]
-        ans1,len1 = self.longestPalinOdd(s,n)
-        ans2,len2 = self.longestPalinEven(s,n)
-        if len1 >= len2:
-            return ans1
-        return ans2
+        if n == 1: return s
+        output = ""
+        for i in range(n - 1):
+            ans1 = helper(s, i, i)
+            ans2 = helper(s, i, i + 1)
+            n1,m1,m2 = len(output),len(ans1),len(ans2)
+            if m1 >= m2 and m1 > n1:
+                output = ans1
+            elif m2 > n1:
+                output = ans2
+        return output
