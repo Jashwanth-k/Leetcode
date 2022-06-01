4d127596862140b8581890b8bf506842cde0e508
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-06-01 12:17:33 +0530

    Time: 900 ms (18.01%), Space: 33.7 MB (78.35%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -1,18 +1,21 @@
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        helper = []
+        maxHeap = []
         for size in range(1,n+1):
             i = 0
             currSum = 0
             for j in range(n):
                 currSum += nums[j]
                 if (j-i+1) == size:
-                    helper.append(currSum)
+                    if len(maxHeap) == right:
+                        if -maxHeap[0] > currSum:
+                            heapq.heapreplace(maxHeap, -currSum)
+                    else:
+                        heapq.heappush(maxHeap,-currSum)
                     currSum -= nums[i]
                     i += 1
         
-        helper.sort()
         ans = 0
         for i in range(left-1,right):
-            ans = (ans + helper[i]) % (10**9 + 7)
+            ans = (ans + -heapq.heappop(maxHeap)) % (10**9 + 7)
         return ans
