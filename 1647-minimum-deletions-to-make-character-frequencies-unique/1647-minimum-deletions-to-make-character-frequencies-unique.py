6f1ff322f3e4ec78d958d888e871172833aedd33
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-06-14 22:52:54 +0530

    Time: 394 ms (21.56%), Space: 14.9 MB (17.09%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,39 +1,17 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        def binary_search(nums,target):
-            si = 0
-            ei = len(nums)-1
-            while si <= ei:
-                mid = si + (ei-si) // 2
-                if nums[mid] == target:
-                    return mid
-                elif target > nums[mid]:
-                    si = mid+1
-                else:
-                    ei = mid-1
-            return ei
-        
-        d = {}
-        maxim = 0
+        frequencies = [0]*26
         for i in s:
-            d[i] = d.get(i,0) + 1
+            frequencies[ord(i)-97] += 1
             
-        freq = {}
-        for i in d:
-            freq[d[i]] = freq.get(d[i],-1) + 1
-            if freq[d[i]] > 0:
-                maxim = max(maxim,d[i])
-        
-        helper = []
-        for j in range(1,maxim+1):
-            if j not in freq:
-                helper.append(j)
-        
-        ans = 0
-        for slen in freq:
-            curr = freq[slen]
-            while curr != 0:
-                idx = binary_search(helper,slen)
-                ans += slen - helper.pop(idx) if idx >= 0 else slen - 0
-                curr -= 1
-        return ans
+        deleteCount = 0
+        frequencies.sort(reverse=True)
+        for j in range(1, 26):
+            if frequencies[j] >= frequencies[j-1]:
+                if frequencies[j-1] == 0:
+                    deleteCount += frequencies[j]
+                    frequencies[j] = 0
+                else:
+                    deleteCount += frequencies[j] - (frequencies[j-1] - 1)
+                    frequencies[j] = frequencies[j-1] - 1
+        return deleteCount
