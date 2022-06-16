43c3cad3a24eb27b59e1962584991cf2ddebbc59
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-06-16 21:05:43 +0530

    Time: 88 ms (31.16%), Space: 14 MB (10.40%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,34 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def countRotten():
-            rotten = []
-            for i in range(n):
-                for j in range(m):
-                    if (i,j) not in used and grid[i][j] == 2:
-                        rotten.append([i,j])
-            return rotten
-        
         n,m = len(grid),len(grid[0])
         minutes = 0
-        used = set()
-        rotten = countRotten()
-
-        while rotten:
-            flag = False
-            for i,j in rotten:
-                used.add((i,j))
-                paths = [[i,j+1],[i,j-1],[i+1,j],[i-1,j]]
-                for r,c in paths:
-                    if 0 <= r < n and 0 <= c < m and grid[r][c] == 1: 
-                        grid[r][c] = 2
-                        flag = True
-            if flag:
-                minutes += 1
-            rotten = countRotten()
+        fresh = 0
         
+        q = collections.deque()
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == 1:
-                    return -1
-        return minutes
+                if grid[i][j] == 2:
+                    q.append([i,j])
+                elif grid[i][j] == 1:
+                    fresh += 1
+        q.append((None,None))
                     
+        while len(q) != 0:
+            r,c = q.popleft()
+            if r == None:
+                minutes += 1
+                if len(q) == 0:
+                    break
+                q.append((None,None))
+                continue
+            
+            paths = [[r,c+1],[r,c-1],[r-1,c],[r+1,c]]
+            for i,j in paths:
+                if 0 <= i < n and 0 <= j < m and grid[i][j] == 1:
+                    fresh -= 1
+                    grid[i][j] = 2
+                    q.append([i,j])
+        return minutes-1 if fresh == 0 else -1
