1f84c5ef30ce9f8b2ce54b31c94b341558942abd
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-06-25 11:59:58 +0530

    Time: 206 ms (82.55%), Space: 15.3 MB (49.84%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -1,32 +1,17 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        n = len(nums)
-        def left_right(nums):
-            flag = False
+        def checkArr(arr):
             for i in range(n-1):
-                if i == 0:
-                    if nums[i] > nums[i+1]:
-                        nums[i] = nums[i+1]
-                        flag = True
-                    continue
-                if nums[i] > nums[i+1]:
-                    nums[i+1] = nums[i]
-                    if flag: return False
-                    flag = True
+                if arr[i] > arr[i+1]:
+                    return False
             return True
         
-        def right_left(nums):
-            flag = False
-            for i in range(n-1,-1,-1):
-                if i == n-1:
-                    if nums[i] < nums[i-1]:
-                        nums[i] = nums[i-1]
-                        flag = True
-                    continue
-                if nums[i] > nums[i+1]:
-                    nums[i] = nums[i+1]
-                    if flag: return False
-                    flag = True
-            return True
-        return left_right(nums[:]) or right_left(nums[:])
-                    
+        n = len(nums)
+        left = nums[:]
+        right = nums[:]
+        for i in range(n-1):
+            if nums[i] > nums[i+1]:
+                left[i] = nums[i+1]
+                right[i+1] = nums[i]
+                return checkArr(left) or checkArr(right)
+        return True
