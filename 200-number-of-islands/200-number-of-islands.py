b8792956d76a6a48dcae46bce0173c2e934df6cd
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-07-01 13:08:29 +0530

    Time: 327 ms (87.56%), Space: 21.7 MB (30.65%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,11 +1,15 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def DFS(r,c):
-            visited.add((r,c))
-            paths = [[r,c+1],[r,c-1],[r+1,c],[r-1,c]]
-            for i,j in paths:
-                if 0 <= i < m and 0 <= j < n and grid[i][j] == "1" and (i,j) not in visited:
-                    DFS(i,j)
+        def BFS(r,c):
+            q = collections.deque()
+            q.append([r,c])
+            while len(q) != 0:
+                r,c = q.popleft()
+                paths = [[r,c+1],[r,c-1],[r+1,c],[r-1,c]]
+                for i,j in paths:
+                    if 0 <= i < m and 0 <= j < n and grid[i][j] == "1" and (i,j) not in visited:
+                        visited.add((i,j))
+                        q.append([i,j])
                     
         m,n = len(grid),len(grid[0])
         visited = set()
@@ -13,6 +17,7 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "0" or (i,j) in visited: continue
-                DFS(i,j)
+                BFS(i,j)
+                visited.add((i,j))
                 ans += 1
         return ans  
