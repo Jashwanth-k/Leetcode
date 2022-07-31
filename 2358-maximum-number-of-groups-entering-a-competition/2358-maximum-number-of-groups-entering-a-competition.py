fdab99721e97d3fbb5c67803e383e233127585fc
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-07-31 10:08:38 +0530

    Time: 744 ms (50.00%), Space: 27.4 MB (16.67%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.py
@@ -1,13 +1,14 @@
 class Solution:
     def maximumGroups(self, grades: List[int]) -> int:
-        grades.sort()
         n = len(grades)
-        length = 1
-        i = 0
-        while i < n:
-            for k in range(i,i+length):
-                if k >= n:
-                    return length-1
-            i += length
-            length += 1
-        return length-1
+        si,ei = 0,n
+        while si <= ei:
+            mid = si + (ei - si) // 2
+            curlen = mid * (mid+1) // 2
+            if curlen == n:
+                return mid
+            if curlen > n:
+                ei = mid-1
+            else:
+                si = mid+1
+        return ei
