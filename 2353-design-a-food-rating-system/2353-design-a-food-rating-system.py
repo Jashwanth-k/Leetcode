c1e5500b478ad859054206eb45d3a33a4bf2bbee
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-08-04 13:59:25 +0530

    Time: 1750 ms (38.49%), Space: 44.1 MB (68.84%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -2,37 +2,22 @@ from sortedcontainers import SortedList
 class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.data = defaultdict(dict)
-        self.sortord = defaultdict(SortedList)
-        self.prev = {}
+        self.data = defaultdict(SortedList)
+        self.findstyle = {}
         for i in range(len(foods)):
-            food = foods[i]
-            country = cuisines[i]
-            rate = ratings[i]
-            if rate not in self.data[country]:
-                self.data[country][rate] = SortedList()
-                self.sortord[country].add(rate)
-            self.data[country][rate].add(food)
-            self.prev[food] = [country,rate]
-        return 
-
-    def changeRating(self, food: str, newRating: int) -> None:
-        country,prevrate = self.prev[food]
-        self.data[country][prevrate].discard(food)
-        if len(self.data[country][prevrate]) == 0:
-            self.data[country].pop(prevrate)
-            self.sortord[country].discard(prevrate)
-        self.prev[food][1] = newRating
-        
-        if newRating not in self.data[country]:
-            self.data[country][newRating] = SortedList()
-            self.sortord[country].add(newRating)
-        self.data[country][newRating].add(food)
+            food,style,rate = foods[i],cuisines[i],ratings[i]
+            self.data[style].add((-rate,food))
+            self.findstyle[food] = [style,rate]
         return
 
+    def changeRating(self, food: str, newRating: int) -> None:
+        style,prevrate = self.findstyle[food]
+        self.data[style].discard((-prevrate,food))
+        self.data[style].add((-newRating,food))
+        self.findstyle[food][1] = newRating
+
     def highestRated(self, cuisine: str) -> str:
-        highval = self.sortord[cuisine][-1]
-        return self.data[cuisine][highval][0]
+        return self.data[cuisine][0][1]
 
 # Your FoodRatings object will be instantiated and called as such:
 # obj = FoodRatings(foods, cuisines, ratings)
