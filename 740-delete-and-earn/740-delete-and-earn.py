a41ad37881ca272c047127915b4df5a1eac31753
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-08-09 18:37:11 +0530

    Time: 145 ms (48.90%), Space: 16.1 MB (61.04%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,22 +1,18 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        def DFS(i):
-            if i >= n:
-                return 0
-            if i == n-1:
-                return nums[i] * d[nums[i]]
-            if dp[i] != -1:
-                return dp[i]
-            
-            inclu = nums[i] * d[nums[i]] + DFS(i+2) if nums[i+1] == nums[i]+1 else nums[i] * d[nums[i]] + DFS(i+1)
-            exclu = DFS(i+1)
-            dp[i] = max(inclu,exclu)
-            return dp[i]
-        
         d = Counter()
         for i in nums:
             d[i] += 1
         nums = sorted(d.keys())
         n = len(nums)
-        dp = [-1]*n
-        return DFS(0)
+        nums.append(-1)
+        p1 = p2 = 0
+        
+        for i in range(n-1,-1,-1):            
+            inclu = nums[i] * d[nums[i]]
+            inclu += p2 if nums[i+1] == nums[i]+1 else p1
+            exclu = p1
+            curr = max(inclu,exclu)
+            p2 = p1
+            p1 = curr
+        return max(p1,p2)
