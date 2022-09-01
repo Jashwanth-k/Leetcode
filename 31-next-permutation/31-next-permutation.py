ad883726233c1922ab9f767589a61ee4bfa6e53f
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-09-01 15:51:41 +0530

    Time: 51 ms (80.60%), Space: 14 MB (24.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,23 +1,24 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
+        def reverse(si,ei):
+            while si <= ei:
+                nums[si],nums[ei] = nums[ei],nums[si]
+                si += 1
+                ei -= 1
+                
         n = len(nums)
-        for i in range(n-1,-1,-1):
-            cur,idx = 1e4,None
-            for j in range(i+1,n):
-                if nums[j] > nums[i] and nums[j] < cur:
-                    cur = nums[j]
-                    idx = j
-                    
-            if idx != None and cur > nums[i]:
-                nums[i],nums[idx] = nums[idx],nums[i]
-                for k in range(i+1,n):
-                    mi = k
-                    for l in range(k+1,n):
-                        if nums[l] < nums[mi]:
-                            mi = l
-                    nums[k],nums[mi] = nums[mi],nums[k]
+        flag = True
+        for i in range(n-1,0,-1):
+            if nums[i-1] < nums[i]:
+                flag = False
+                cur,idx = 1e4,None
+                for j in range(i,n):
+                    if nums[j] > nums[i-1] and nums[j] <= cur:
+                        cur = nums[j]
+                        idx = j
+                nums[i-1],nums[idx] = nums[idx],nums[i-1]
+                reverse(i,n-1)
                 break
-        if idx == None:
-            for i in range(((n-1)//2)+1):
-                nums[i],nums[n-1-i] = nums[n-1-i],nums[i]
+        if flag:
+            reverse(0,n-1)
         return
