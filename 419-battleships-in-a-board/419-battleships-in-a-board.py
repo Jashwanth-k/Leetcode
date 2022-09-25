f04f3122691a525614926f51b0a96a1a2d174886
Jashwanth-k <89581005+Jashwanth-k@users.noreply.github.com>
2022-09-25 19:54:53 +0530

    Time: 163 ms (23.44%), Space: 14.6 MB (22.02%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -1,17 +1,9 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
-        def DFS(i,j):
-            board[i][j] = "."
-            for r,c in [[0,1],[0,-1],[1,0],[-1,0]]:
-                ni,nj = i + r,j + c
-                if 0 <= ni < n and 0 <= nj < m and board[ni][nj] == "X":
-                    DFS(ni,nj)
-            
         n,m = len(board),len(board[0])
         res = 0
         for i in range(n):
             for j in range(m):
-                if board[i][j] == "X":
-                    DFS(i,j)
+                if (i-1 < 0 or board[i-1][j] == ".") and (j-1 < 0 or board[i][j-1] == ".") and board[i][j] == "X":
                     res += 1
         return res
